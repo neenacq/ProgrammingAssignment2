32a9cc67f2980c2c73087869877ba1dd32b119f4
neenacq <neenadavies@Macintosh.local>
2014-08-22 09:47:12 -0400

    Coursera Project R Program 002
    Writing lexical scoping functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Neena Davies/19 August 2014
+## 
+## These functions are for Coursework 002 of ProgramR with coursera.
+## Please note that I use both <- and = for assignment based on experience
+## in other projects.  They have the same properties. 
 
-## Write a short comment describing this function
-
+## Brief Description of makeCacheMatrix
+## makeCacheMatrix performs set, get, setInverse and getInverse actions 
+## on the matrix passed in.  Note that y is the variable that can retain
+## it's value via lexical scoping.
 makeCacheMatrix <- function(x = matrix()) {
+    
+    m <- NULL
+    
+    #set y to x and assign m as null, not the <<- operator here
+    #this means that a value can be assigned from a different environment
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    #get function will return x
+    get <- function() x
+    
+    #This is the inverse matrix method solve
+    setInverse <- function(matrix) m <<- solve(matrix)
+    
+    #Get inverse returns m
+    getInverse <- function() m
+    
+    #The last command is what is returned when the function is called.
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Brief Description of cacheSolve
+## cacheSolve checks that the matrix doesn't exist already and then 
+## creates it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+
+    # First check if the matrix is cached already
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    
+    #If m isn't cached then call the get function to get the value
+    # and setInverse 
+    data <- x$get()
+    m <- data
+    x$setInverse(m)
+    
+    m
 }
